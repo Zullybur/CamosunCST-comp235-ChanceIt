241eb7f47f1961f96313b8fb2e7969d215ddc864
Chris Wong <wong.c1125@gmail.com>
2016-05-11 14:47:58 -0700

    Added the dice.h and networkProtocol.h

---
+++ interfaces/dice.h
@@ -0,0 +1,8 @@
+//assigns the die with a random number
+//Pre-Conditions: Must be during a turn
+//Post-Conditions: A number random number was generated 
+//Clean-Up: N/A
+//Returns: The random number
+unsigned rollDie(){
+	
+}
