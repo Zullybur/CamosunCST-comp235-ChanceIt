57c15359d23417951b8ec9bd29bfcf99d16e547c
sherylltabamo <tsheryll0@gmail.com>
2016-05-11 14:41:18 -0700

    add probability.h

---
+++ interfaces/probability.h
@@ -0,0 +1,14 @@
+/*Pre-condition: N/A
+ *Post-condition: returns the probability and outputs on screen
+ *Clean up: N/A
+ *Param @sum 
+ *returns probability
+ */
+
+#include<stdio.h>
+
+double getProbability(int sum);
+
+double calcProbability(int sum);
+
+
