484edd7835040d07d608bea903d6c19f82872668
sherylltabamo <tsheryll0@gmail.com>
2016-05-11 21:12:04 -0700

    add inputoutput.h

---
+++ interfaces/inputOutput.h
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+/*Pre-condition: N/A
+ *Post-condition: The screen was updated to display new information
+ *Clean up: N/A
+ *Param name, userSelection 
+ *returns N/A
+ */
+void dispGamePlay(char *name, int userSelection); 
+
+/*Pre-condition: A highscore file exists
+ *Post-condition: The highscore was displayed on the screen
+ *Clean up: N/A
+ *Param N/A
+ *returns N/A
+ */
+void dispHighScore()
+
+/*Pre-condition: N/A
+ *Post-condition: The main menu was displayed on the screen
+ *Clean up: N/A
+ *Param N/A
+ *returns N/A
+ */
+void dispMainMenu()
+
+/*Pre-condition: N/A
+ *Post-condition: The help menu was displayed on the screen
+ *Clean up: N/A
+ *Param N/A
+ *returns N/A
+ */
+void dispHelpMenu()
+
+/*Pre-condition: N/A
+ *Post-condition: The Network Play Mode was displayed on screen
+ *Clean up: N/A
+ *Param N/A
+ *returns N/A
+ */
+void dispNetworkPlayMode()
+
+/*Pre-condition: N/A
+ *Post-condition: The prompt for Network information was displayed on screen
+ *Clean up: N/A
+ *Param N/A
+ *returns N/A
+ */
+void dispNetWorkPlayInput()
+
+/*Pre-condition: N/A
+ *Post-condition: The local Play Mode was displayed on screen
+ *Clean up: N/A
+ *Param N/A
+ *returns N/A
+ */
+void dispLocalPlayMode()
+
+/*Pre-condition: N/A
+ *Post-condition: The menu for game help was displayed on screen
+ *Clean up: N/A
+ *Param N/A
+ *returns N/A
+ */
+void dispInGameHelpMenu()
+
+
+
+
