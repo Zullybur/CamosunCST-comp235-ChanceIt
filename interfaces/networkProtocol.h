558f591f9d724ecf6dd17b60939df674aa6e0d33
Matthew Casiro <Matthew Casiro>
2016-05-17 00:12:56 -0700

    remove exception throwing from network

--- interfaces/networkProtocol.h
+++ interfaces/networkProtocol.h
@@ -2,5 +2,4 @@
 // Pre: A network game is chosen
 // Post: A connection to a server was made
 // Clean-Up: Close connection after the game is finished
-// Throws: connection error if unable to connect to the server
 void connectInit(char* IPaddress, int port);
