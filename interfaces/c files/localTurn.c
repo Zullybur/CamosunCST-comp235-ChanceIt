93dc4a5c679ac809117b89122871efe5b73d339c
Melissa P <73hl10n@gmail.com>
2016-05-29 10:48:24 -0700

    finish prototype of local turn

---
+++ interfaces/c files/localTurn.c
@@ -0,0 +1,37 @@
+// values passed via game module
+char* name
+char* opponentName
+unsigned opponentScore
+// local turn variables
+unsigned roundCounter;
+unsigned firstRoll;
+unsigned turnScore;
+unsigned die1;
+unsigned die2;
+char response;
+
+// while we are not going to (f)orefit
+if (response != "f") {
+  // initial roll
+  die1 = roll();
+  die2 = roll();
+  firstRoll = die1 + die2;
+  roundScore = firstRoll;
+  roundCoutner++;
+  displayTurn(name, firstRoll, roundCoutner, roundScore, die1, die2, turnScore, opponentName, opponentScore);
+  response = getChar();
+
+  // consecutive rolls while we're not (s)topping
+  while (response != "s") {
+    die1 = roll();
+    die2 = roll();
+    roundScore = die1 + die2;
+    turnScore += roundScore;
+    displayTurn(name, firstRoll, roundCoutner, roundScore, die1, die2, turnScore, opponentName, opponentScore);
+    response = getChar();
+    roundCoutner++;
+  }
+ 
+}
+// finish turn
+return turnScore;
