9a3ef53b0b4072e392e71d13d45e138640486110
Zullybur <mattcasiro@gmail.com>
2016-05-29 12:04:41 -0700

    pre-merge

---
+++ interfaces/dice-Matthew’s MacBook Pro.h
@@ -0,0 +1,6 @@
+// Assigns the die with a random number
+// Pre-Conditions: Must be during a turn
+// Post-Conditions: A random number was generated 
+// Clean-Up: N/A
+// Returns: The random number
+int rollDie();
