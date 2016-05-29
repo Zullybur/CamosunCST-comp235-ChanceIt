3140b07eb50bcabddc17e7bb031a2ed46e111313
Melissa P <73hl10n@gmail.com>
2016-05-29 11:39:22 -0700

    Add things I forgot about

--- interfaces/c files/localTurn.c
+++ interfaces/c files/localTurn.c
@@ -1,6 +1,8 @@
 #include "localTurn.h";
 #include "probability.h";
 #include "inputOutput.h";
+#include <stdlib.h>
+#include <stdio.h>
 
 // values passed via game module
 char* name
@@ -10,6 +12,7 @@ unsigned opponentScore
 // local turn variables
 unsigned roundCounter = 1;
 unsigned firstRoll;
+unsigned roundScore;
 unsigned turnScore;
 unsigned die1;
 unsigned die2;
@@ -19,8 +22,17 @@ do {
   // roll and calcualte
   die1 = roll();
   die2 = roll();
-  firstRoll = die1 + die2;
-  roundScore = firstRoll;
+  if (roundCoutner == 1) {
+    firstRoll = die1 + die2;
+  }
+  roundScore = die1 + die2;
+  turnScore += roundScore;
+
+  // if you lose by rolling
+  if (roundScore == firstRoll) {
+    displayGameOver();
+    return turnScore;
+  }
 
   displayTurn(name, firstRoll, roundCoutner, roundScore, die1, die2, turnScore,
               opponentName, opponentScore);
@@ -35,6 +47,7 @@ do {
         break;
       // forfeit the game
       case "f":
+        displayGameOver();
         return turnScore;
       // get the probability
       case "p":
@@ -53,3 +66,6 @@ do {
   // increment and continue if applicable
   roundCoutner++;
 } while (response == "s");
+
+// finish turn
+return turnScore;
