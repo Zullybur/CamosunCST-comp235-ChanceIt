b800bbbbcb01644a5523f620a1d41a7819bbd016
Zullybur <mattcasiro@gmail.com>
2016-05-30 12:15:31 -0700

    updated signature

--- Code/interfaces/game.h
+++ Code/interfaces/game.h
@@ -2,6 +2,7 @@
 // Pre: randomInit() has been called once
 // Post: N/A
 // Clean-Up: N/A
-// Param: player is a pointer to an unsigned variable
+// Param: opponentLocal is 0 for local play, or 1 for network play
+// Param: opponentHuman is 0 for a computer, or 1 for human
 // Return: the winning score of the game
-unsigned gameInit();
+void gameInit(_Bool opponentLocal, _Bool opponentHuman);
