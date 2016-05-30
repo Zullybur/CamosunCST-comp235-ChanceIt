3d79831a78b3150e0bca572021c0fdbeaa372409
Zullybur <mattcasiro@gmail.com>
2016-05-30 09:07:12 -0700

    placeholders for param descriptions added

--- Code/interfaces/localTurn.h
+++ Code/interfaces/localTurn.h
@@ -2,5 +2,10 @@
 // Pre: N/A
 // Post: N/A
 // Clean-Up: N/A
+// Param: player
+// Param: playerName
+// Param: opponentName
+// Param: p1Score
+// Param: p2Score
 // Returns: the final turn score p
 unsigned localTurn(unsigned player, char* playerName, char* opponentName, unsigned p1Score, unsigned p2Score);
