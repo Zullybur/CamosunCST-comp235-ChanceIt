42b651c61252952436d6d4f04ade7086135b7572
Zullybur <mattcasiro@gmail.com>
2016-05-30 15:01:09 -0700

    game compiled

--- Code/c files/game.c
+++ Code/c files/game.c
@@ -2,45 +2,8 @@
 #include "../interfaces/inputOutput.h"
 #include "../interfaces/localTurn.h"
 
-#define OPPONENT_ERROR "The selected opponent type was invalid."
 #define MAX_NAME_LENGTH 16
 #define ROUNDS 20
-
-// Initiate a new game of Chance-It
-// Pre: randomInit() has been called once
-// Post: N/A
-// Clean-Up: N/A
-// Param: player is a pointer to an unsigned variable
-// Return: the winning score of the game
-void gameInit(_Bool opponentLocal, _Bool humanFactor)
-{
-    // Prepare persistent arrays for player names
-    char player1Name[MAX_NAME_LENGTH];
-    // Get Player Names
-
-    if(!opponentLocal)
-    {
-        char player2Name[] = "Network Player";
-        player1Name = displayLocalPlayGetName(player1Name, player2Name, humanFactor);
-    } else if (!humanFactor) {
-        char player2Name[] = "Computer AI";
-        player1Name = displayLocalPlayGetName(player1Name, player2Name, humanFactor);
-    } else {
-        char player2Name[MAX_NAME_LENGTH] = displayLocalPlayGetName(player1Name, player2Name, humanFactor);
-    }
-    // Run local vs network game
-    switch (opponentLocal)
-    {
-        case 0:
-            playNetwork();
-            break;
-        case 1:
-            playLocal(humanFactor, player1, player2);
-            break;
-        case default:
-            printf("%s\n", OPPONENT_ERROR);
-    }
-}
 
 static void playLocal(_Bool humanFactor, char* player1Name, char* player2Name)
 {
@@ -77,4 +40,29 @@ static void playLocal(_Bool humanFactor, char* player1Name, char* player2Name)
 static void playNetwork()
 {
     printf("Network Play has not been implemented.\n");
+}
+
+void gameInit(_Bool opponentLocal, _Bool humanFactor)
+{
+    // Prepare persistent arrays for player names
+    char player1Name[MAX_NAME_LENGTH];
+    char player2Name[MAX_NAME_LENGTH];
+    // Get Player Names
+    if(!opponentLocal)
+    {
+        char player2Name[] = "Network Player";
+    } else if (!humanFactor) {
+        char player2Name[] = "Computer AI";
+    } else {
+        char player2Name[MAX_NAME_LENGTH];
+    }
+    displayLocalPlayGetName(player1Name, player2Name, humanFactor);
+
+    // Run local vs network game
+    if (opponentLocal)
+    {
+        playLocal(humanFactor, player1Name, player2Name);
+    } else {
+        playNetwork();
+    }
 }
