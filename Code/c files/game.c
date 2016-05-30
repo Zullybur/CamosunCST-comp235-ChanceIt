103c61a5411a13d8f484028ff097c1fe77f95fe9
Zullybur <mattcasiro@gmail.com>
2016-05-30 14:47:06 -0700

    version 0.1 of game, local play only

--- Code/c files/game.c
+++ Code/c files/game.c
@@ -12,11 +12,22 @@
 // Clean-Up: N/A
 // Param: player is a pointer to an unsigned variable
 // Return: the winning score of the game
-void gameInit(_Bool opponentLocal, _Bool opponentHuman)
+void gameInit(_Bool opponentLocal, _Bool humanFactor)
 {
     // Prepare persistent arrays for player names
-    char player1[MAX_NAME_LENGTH];
-    char player2[MAX_NAME_LENGTH];
+    char player1Name[MAX_NAME_LENGTH];
+    // Get Player Names
+
+    if(!opponentLocal)
+    {
+        char player2Name[] = "Network Player";
+        player1Name = displayLocalPlayGetName(player1Name, player2Name, humanFactor);
+    } else if (!humanFactor) {
+        char player2Name[] = "Computer AI";
+        player1Name = displayLocalPlayGetName(player1Name, player2Name, humanFactor);
+    } else {
+        char player2Name[MAX_NAME_LENGTH] = displayLocalPlayGetName(player1Name, player2Name, humanFactor);
+    }
     // Run local vs network game
     switch (opponentLocal)
     {
@@ -24,37 +35,43 @@ void gameInit(_Bool opponentLocal, _Bool opponentHuman)
             playNetwork();
             break;
         case 1:
-            playLocal(opponentHuman, player1, player2);
+            playLocal(humanFactor, player1, player2);
             break;
         case default:
             printf("%s\n", OPPONENT_ERROR);
     }
-    // While turns < 20, run turns
-            // If F, break
-
-        // Display win/lose page
-            // If ??? continue
-            // If ??? break
-    }
-    // Thanks for playing bye.
-
 }
 
-static void playLocal(_Bool opponentHuman, char* player1, char* player2)
+static void playLocal(_Bool humanFactor, char* player1Name, char* player2Name)
 {
     unsigned p1Score = 0;
     unsigned p2Score = 0;
+    int tmpResult;
 
-    // Get Player Names
-    void displayLocalPlayGetName(player1, player2, opponentHuman);
-    // Run <ROUNDS> turns
-    for (unsigned i = 0; i < ROUNDS; i++)
+    // Run <ROUNDS> turns for each player
+    for (unsigned i = 0; i < ROUNDS * 2; )
     {
         // Call first player's turn
-        localTurn()
+        tmpResult = localTurn(humanFactor, player1Name, player2Name, p1Score, p2Score, i++);
+        if (tmpResult >= 0)
+        {
+            p1Score += tmpResult;
+        } else {
+            printf("Player 1 Forfeits TODO: Game over screen\n");
+            break;
+        }
+
         // Call second player's turn
+        tmpResult = localTurn(humanFactor, player1Name, player2Name, p1Score, p2Score, i++);
+        if (tmpResult >= 0)
+        {
+            p2Score += tmpResult;
+        } else {
+            printf("Player 2 Forfeits TODO: Game over screen\n");
+            break;
+        }
     }
-    
+    printf("TODO: Game over screen\n");
 }
 
 static void playNetwork()
