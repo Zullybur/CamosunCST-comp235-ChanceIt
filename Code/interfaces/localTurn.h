2d033d747a466bea6bc22b3413ace5b1d0ef0639
Melissa P <73hl10n@gmail.com>
2016-05-30 08:57:52 -0700

    Updated signature

--- Code/interfaces/localTurn.h
+++ Code/interfaces/localTurn.h
@@ -2,5 +2,5 @@
 // Pre: N/A
 // Post: N/A
 // Clean-Up: N/A
-// Returns: the final turn score
-unsigned localTurn();
+// Returns: the final turn score p
+unsigned localTurn(unsigned player, char* playerName, char* opponentName, unsigned p1Score, unsigned p2Score);
