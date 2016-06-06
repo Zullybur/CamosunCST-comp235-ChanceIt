7f99378d4728548ef0c751e05db6ffda7ac73089
Zullybur <mattcasiro@gmail.com>
2016-06-06 16:24:30 -0700

    added struct for networkTurn.h

--- Code/interfaces/networkTurn.h
+++ Code/interfaces/networkTurn.h
@@ -1,6 +1,17 @@
+typedef struct NetworkTurn {
+    isHuman,
+    p1Name,
+    p2Name,
+    isFirst,
+    localScore,
+    opponentScore,
+    turnNum,
+    firstRoll
+} NetworkTurn;
+
 // Recieves data from the network player
 // Pre: N/A
 // Post: N/A
 // Cleanup N/A
 // Return an unsigned of the turn score
-unsigned networkTurn();
+unsigned networkTurn(NetworkTurn* params);
